d768dc638f967f263dab002bffa93ced68758bcf
hajarash <hajarash@students.cs.ubc.ca>
2019-10-10 20:48:27 -0700

    Merge branch 'master' of https://github.com/goro341/cpsc410-dsl
    # Conflicts:
    #	src/dsl/ast/CREATE.tsx
    #	src/dsl/ast/obj/ObjectNode.ts

--- src/dsl/ast/ADD.tsx
+++ src/dsl/ast/ADD.tsx
@@ -1,7 +1,6 @@
-import ASTNode from "./ASTNode";
 import * as React from "react";
 import STATEMENT from "./STATEMENT";
-import Tokenizer from "../libs/Tokenizer";
+import ParsingException from "../exception/ParsingException";
 
 /**
  * Represents
@@ -9,10 +8,23 @@ import Tokenizer from "../libs/Tokenizer";
  *
  * Will lookup x x2 x3 in the symbols table and add them to y
  */
-export default class ADD extends STATEMENT {
+export default class ADD extends STATEMENT{
+    private child :  string;
+    private parent: string;
 
+    constructor() {
+        super();
+        this.child = "";
+        this.parent = "";
+    }
     public parseNode(): void {
         ADD.tokenizer.getAndCheckNext('/ADD/g');
+        this.child = ADD.tokenizer.getNext();
+        let to:any = ADD.tokenizer.getNext();
+        if (to === "!to") {
+            throw new ParsingException();
+        }
+        this.parent = ADD.tokenizer.getNext();
     }
 
     public evaluateNode(): void {
