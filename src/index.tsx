0e2956572098c1e02e5d9872722f5e850ba5c5d7
goro341 <masoudjaveri@yahoo.com>
2019-10-10 20:41:46 -0700

    sets Tokenizer and literals and input file path

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import PROGRAM from "./dsl/ast/PROGRAM";
+import Tokenizer from "./dsl/libs/Tokenizer";
 
 console.log("this is the main function");
+
+const literals = ["CREATE", "ADD", "PAGE", "COMPONENT", "PHOTO", "TABLE", "ROW", "to"];
+const userInputFilePath = "src/dsl/user-input.txt";
+Tokenizer.makeTokenizer(userInputFilePath, literals);
 const program = new PROGRAM();
 program.parseNode();
 const root: JSX.Element = program.evaluateNode();
